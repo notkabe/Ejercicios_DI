121e4081af27e2c272b4932726475660d6abdc76
notkabe <yeraydp12@gmail.com>
2025-03-13 12:42:07 +0100

    Add files via upload

---
+++ ejercicio5/Form1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new ejercicio5.UserControl1();
+            this.userControl12 = new ejercicio5.UserControl1();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(103, 110);
+            this.userControl11.Multilinea = false;
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(144, 40);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.Texto = "";
+            this.userControl11.Tipo = ejercicio5.UserControl1.eTipo.Textual;
+            // 
+            // userControl12
+            // 
+            this.userControl12.Location = new System.Drawing.Point(103, 192);
+            this.userControl12.Multilinea = false;
+            this.userControl12.Name = "userControl12";
+            this.userControl12.Size = new System.Drawing.Size(144, 40);
+            this.userControl12.TabIndex = 1;
+            this.userControl12.Texto = "";
+            this.userControl12.Tipo = ejercicio5.UserControl1.eTipo.Numérico;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Textual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numérico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userControl12);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ejercicio5.UserControl1 userControl11;
+        private ejercicio5.UserControl1 userControl12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ejercicio5/ejercicio5/UserControl1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio5
+{
+    public partial class UserControl1 : UserControl
+    {
+        Color color;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            this.Width = 100;
+            textBox1.Height = 30;
+        }
+
+        private void Component_Resize(object sender, EventArgs e)
+        {
+            this.Height = textBox1.Height + 20;
+            textBox1.Width = this.Width - 20;
+        }
+
+        public event EventHandler TxtChanged;
+
+        protected virtual void OnTxtChanged()
+        {
+            if (tipo == eTipo.Numérico)
+            {
+                if (int.TryParse(textBox1.Text, out int num))
+                {
+                    color = Color.Green;
+                }
+                else
+                {
+                    color = Color.Red;
+                }
+            }
+            else
+            {
+                if (!textBox1.Text.Any(char.IsDigit))
+                {
+                    color = Color.Green;
+                }
+                else
+                {
+                    color = Color.Red;
+                }
+            }
+
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                color = BackColor;
+            }
+
+            Refresh();
+
+            TxtChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            OnTxtChanged();
+        }
+
+        private string texto = "";
+        [Category("AAA Mis Propiedades")]
+        [Description("Cambia el texto del textbox")]
+        public string Texto
+        {
+            set => textBox1.Text = value;
+            get => textBox1.Text;
+        }
+
+        private bool multilinea = false;
+        [Category("AAA Mis Propiedades")]
+        [Description("Indica si permite o no multilinea en el textbox")]
+        public bool Multilinea
+        {
+            set => multilinea = value;
+            get => multilinea;
+        }
+
+        public enum eTipo { Numérico, Textual }
+        private eTipo tipo = eTipo.Numérico;
+        [Category("AAA Mis Propiedades")]
+        [Description("Indica el tipo de textbox a comprobar")]
+        public eTipo Tipo
+        {
+            set => tipo = value;
+            get => tipo;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            using (Pen pen = new Pen(color, 1))
+            {
+                g.DrawRectangle(pen, new Rectangle(5, 5, this.Width - 10, this.Height - 10));
+            }
+        }
+    }
+                //g.DrawRectangle(pen, new Rectangle(this.Width - 5, this.Height - 5, 5, 5));
+}
